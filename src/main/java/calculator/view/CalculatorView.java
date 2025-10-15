7d7083a97ab3ba6e2e2632379a978116c8da4fc6
chocoding <galmeagi2@naver.com>
2025-10-15 20:11:08 +0900

    refactor: 클래스명 수정

--- src/main/java/calculator/view/CalculatorView.java
+++ src/main/java/calculator/view/CalculatorView.java
@@ -1,11 +1,16 @@
 package calculator.view;
 
+import calculator.controller.CalculatorController;
 import camp.nextstep.edu.missionutils.Console;
 
-public class CalculationView {
+public class CalculatorView {
+
+    private final CalculatorController calculatorController = new CalculatorController();
 
     public void startCalculation() {
         String inputStr = inputViewRendering();
+
+        calculatorController.calculate(inputStr);
     }
 
     private String inputViewRendering() {
