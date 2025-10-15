d5025393f5bd4e75bdcac5a989c6eef8632ccfa4
chocoding <galmeagi2@naver.com>
2025-10-15 21:48:34 +0900

    feat: 문자열 분리 기능 구현

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -1,13 +1,16 @@
 package calculator.controller;
 
 import calculator.service.DelimiterService;
+import calculator.service.SeparationService;
 
 public class CalculatorController {
 
     private final DelimiterService delimiterService = DelimiterService.getInstance();
+    private final SeparationService separationService = SeparationService.getInstance();
 
     public int calculate(String inputStr) {
         delimiterService.checkCustomDelimiter(inputStr);
+        int[] numbers = separationService.separate(inputStr);
 
         return 0;
     }
--- src/main/java/calculator/service/SeparationService.java
+++ src/main/java/calculator/service/SeparationService.java
@@ -1,8 +1,15 @@
 package calculator.service;
 
+import static calculator.model.delimiter.DelimiterConstant.CUSTOM_DELIMITER_PREFIX;
+import static calculator.model.delimiter.DelimiterConstant.CUSTOM_DELIMITER_SUFFIX;
+
+import calculator.model.delimiter.Delimiter;
+import java.util.Arrays;
+
 public class SeparationService {
 
     private static SeparationService instance;
+    private final Delimiter delimiter = Delimiter.getInstance();
 
     private SeparationService() {
     }
@@ -14,7 +21,23 @@ public class SeparationService {
         return instance;
     }
 
-    public void separate(String inputStr) {
-        
+    public int[] separate(String inputStr) {
+        if (isContainCustomDelimiter(inputStr)) {
+            inputStr = extractInputStr(inputStr);
+        }
+
+        String delimiters = delimiter.getDelimiters();
+
+        return Arrays.stream(inputStr.split(delimiters))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    private boolean isContainCustomDelimiter(String inputStr) {
+        return inputStr.startsWith(CUSTOM_DELIMITER_PREFIX);
+    }
+
+    private String extractInputStr(String inputStr) {
+        return inputStr.substring(inputStr.indexOf(CUSTOM_DELIMITER_SUFFIX) + 2);
     }
 }
