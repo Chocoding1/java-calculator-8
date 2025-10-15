b32d744a3b72f5d7aa1a6611759d099f11aebb4a
chocoding <galmeagi2@naver.com>
2025-10-15 23:01:21 +0900

    refactor: 정규표현식 사용

--- src/main/java/calculator/model/delimiter/DelimiterConstant.java
+++ src/main/java/calculator/model/delimiter/DelimiterConstant.java
@@ -3,7 +3,7 @@ package calculator.model.delimiter;
 public class DelimiterConstant {
 
     public static final String CUSTOM_DELIMITER_PREFIX = "//";
-    public static final String CUSTOM_DELIMITER_SUFFIX = "\n";
+    public static final String CUSTOM_DELIMITER_SUFFIX = "\\n";
 
     private DelimiterConstant() {
     }
