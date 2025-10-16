ebb8fe27eda24925fcb0b8862e83c139e86b8b68
chocoding <galmeagi2@naver.com>
2025-10-16 12:40:49 +0900

    refactor: View 의존성 추가에 따른 로직 변경

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -2,16 +2,30 @@ package calculator.controller;
 
 import calculator.service.DelimiterService;
 import calculator.service.SeparationService;
+import calculator.view.InputView;
 
 public class CalculatorController {
 
+    private static CalculatorController instance;
+    private final InputView inputView = InputView.getInstance();
     private final DelimiterService delimiterService = DelimiterService.getInstance();
     private final SeparationService separationService = SeparationService.getInstance();
 
-    public int calculate(String inputStr) {
+    private CalculatorController() {
+    }
+
+    public static CalculatorController getInstance() {
+        if (instance == null) {
+            return new CalculatorController();
+        }
+        return instance;
+    }
+
+    public void startApp() {
+        String inputStr = inputView.inputViewRendering();
+
         delimiterService.checkCustomDelimiter(inputStr);
         int[] numbers = separationService.separate(inputStr);
-
-        return 0;
     }
+
 }
