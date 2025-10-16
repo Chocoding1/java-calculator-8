011686b32382935517297c721507ad0a21f1e3f4
chocoding <galmeagi2@naver.com>
2025-10-16 12:36:50 +0900

    refactor: 싱클톤 패턴 도입

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,21 +1,23 @@
 package calculator.view;
 
-import calculator.controller.CalculatorController;
 import camp.nextstep.edu.missionutils.Console;
 
-public class CalculatorView {
+public class InputView {
 
-    private final CalculatorController calculatorController = new CalculatorController();
+    private static InputView instance;
 
-    public void startCalculation() {
-        String inputStr = inputViewRendering();
-
-        calculatorController.calculate(inputStr);
+    private InputView() {
     }
 
-    private String inputViewRendering() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+    public static InputView getInstance() {
+        if (instance == null) {
+            return new InputView();
+        }
+        return instance;
+    }
 
+    public String inputViewRendering() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         return Console.readLine();
     }
 }
