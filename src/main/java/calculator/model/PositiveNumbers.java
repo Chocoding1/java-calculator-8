9fba851539e15e68b5e84ca53be7b22e3c20537e
Chocoding1 <galmeagi2@naver.com>
2025-12-05 16:44:26 +0900

    Refactor(PositiveNumbers): 생성자 로직 수정 & 덧셈 메서드 로직 수정

--- src/main/java/calculator/model/PositiveNumbers.java
+++ src/main/java/calculator/model/PositiveNumbers.java
@@ -2,20 +2,27 @@ package calculator.model;
 
 import java.util.List;
 
+/**
+ * 리팩토링 가이드
+ * https://chatgpt.com/s/t_693287c436808191a1ce205c74c5aef8
+ * https://chatgpt.com/s/t_69328d2c154481919df5b4fb92a13038
+ * https://chatgpt.com/s/t_693288a453dc8191b7ffc5c14e8dae28
+ * https://chatgpt.com/s/t_693288feff388191afb937e2a83787ce
+ * https://chatgpt.com/s/t_6932896922d08191ad5e4a43f54d38b8
+ * https://chatgpt.com/s/t_69328cc157d8819191f5e04b3f39b722
+ * https://chatgpt.com/s/t_69328c9a3edc819188126182765bba7f
+ */
 public class PositiveNumbers {
 
     private final List<PositiveNumber> positiveNumbers;
 
     public PositiveNumbers(List<PositiveNumber> positiveNumbers) {
-        this.positiveNumbers = positiveNumbers;
+        this.positiveNumbers = List.copyOf(positiveNumbers);
     }
 
     public int sum() {
-        int sum = 0;
-
-        for (PositiveNumber positiveNumber : positiveNumbers) {
-            sum += positiveNumber.getNumber();
-        }
-        return sum;
+        return positiveNumbers.stream()
+                .mapToInt(PositiveNumber::getNumber)
+                .sum();
     }
 }
