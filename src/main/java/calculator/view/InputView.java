8cfa788799bb5613d33465fe0a2ed4465ffc1f2f
Chocoding1 <galmeagi2@naver.com>
2025-12-05 17:29:44 +0900

    Refactor(view): 접근제어자 변경

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -2,10 +2,13 @@ package calculator.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+/**
+ * 리팩토링 가이드 https://chatgpt.com/s/t_69329672acfc81918f3725dc384fdcea
+ * https://chatgpt.com/s/t_6932967b344c8191bb123a9b196005b8
+ */
 public class InputView {
 
-    public static final String INPUT_NOTICE = "덧셈할 문자열을 입력해 주세요.";
-
+    private static final String INPUT_NOTICE = "덧셈할 문자열을 입력해 주세요.";
 
     public String getInput() {
         System.out.println(INPUT_NOTICE);
--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,5 +1,10 @@
 package calculator.view;
 
+/**
+ * 리팩토링 가이드
+ * https://chatgpt.com/s/t_693297b9a2cc8191ae62231c486f5e6b
+ * https://chatgpt.com/s/t_693297ca51ac8191884fb90ee1dd3a12
+ */
 public class OutputView {
 
     private static final String OUTPUT_PREFIX = "결과 : ";
