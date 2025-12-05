1f996274f802a8976ea20d733809704e4bc6f940
Chocoding1 <galmeagi2@naver.com>
2025-12-05 15:29:50 +0900

    Docs(CalculatorController): 리팩토링 가이드 링크 추가

--- src/main/java/calculator/controller/CalculatorController.java
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -7,6 +7,12 @@ import calculator.model.PositiveNumbers;
 import calculator.view.InputView;
 import calculator.view.OutputView;
 
+/**
+ * 리팩토링 가이드
+ * https://chatgpt.com/s/t_69327b86bf04819196721febecc2d61c
+ * https://chatgpt.com/s/t_69327baf7cec8191b10369f15b622e0d
+ *
+ */
 public class CalculatorController {
 
     private final InputView inputView;
